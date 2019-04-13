30b3d1e297bc810703e8ed9295a15b48ef5357d4
Igor Gulamov <igor.gulamov@gmail.com>
2019-04-14 01:50:49 +0300

    final field type finished

--- src/ff.jl
+++ src/ff.jl
@@ -1,3 +1,4 @@
+import Base: /, *, +, -
 using BitIntegers
 
 const _P_ = uint256"21888242871839275222246405745257275088548364400416034343698204186575808495617"
@@ -5,23 +6,16 @@ const _P_ = uint256"218882428718392752222464057452572750885483644004160343436982
 mutable struct ff <: Integer
     v::UInt256
     ff(v::T) where T <: Integer = begin 
-        t = mod(v, _P_)
+        t = UInt256(mod(v, _P_))
         return (t >= 0) ? new(t) : new(t + _P_) 
     end 
     ff(v::ff) = v
 end
 
 
-Base.:+(a::ff, b::T) where T <: Integer = Base.:+(a, ff(b))
-Base.:+(a::ff, b::ff) = ff((a.v + b.v + _P_) % _P_)
 
-
-Base.:-(a::ff, b::T) where T <: Integer = Base.:-(a, ff(b))
-Base.:-(a::ff, b::ff)  = ff((a.v - b.v + _P_) % _P_)
-
-
-Base.:*(a::ff, b::T) where T <: Integer = Base.:*(a, ff(b))
-Base.:*(a::ff, b::ff) = ff(widemul(a.v, b.v) % _P_)
-
-Base.:/(a::ff, b::T) where T <: Integer = Base.:/(a, ff(b))
-Base.:/(a::ff, b::ff) = ff(widemul(a.v, invmod(b.v, _P_)) % _P_)
+(+)(a::ff, b::ff) = ff(a.v + b.v)
+(-)(a::ff, b::ff)  = ff(_P_ + a.v - b.v)
+(-)(a::ff)  = ff(_P_ - a.v)
+(*)(a::ff, b::ff) = ff(widemul(a.v, b.v) % _P_)
+(/)(a::ff, b::ff) = ff(widemul(a.v, invmod(b.v, _P_)) % _P_)
